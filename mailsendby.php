f3e1fb65bf6b53157e8e2a1b0e8742b46ecd863f
Oscar Jipiz Castro <32991695+OscarJipiz@users.noreply.github.com>
2017-11-26 11:20:55 -0500

    Add files via upload

--- mailsendby.php
+++ mailsendby.php
@@ -1,65 +1,22 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="utf-8">
-    <title>Confirmacion de envio formulario</title>
-    <link rel="stylesheet" href="assets/css/css.css">
-</head>
-<body>
-<?php
-function form_mail($sPara, $sAsunto, $sTexto, $sDe)
-{
-$bHayFicheros = 0;
-$sCabeceraTexto = "";
-$sAdjuntos = "";
-if ($sDe)$sCabeceras = "From:".$sDe."\n";
-else $sCabeceras = "";
-$sCabeceras .= "MIME-version: 1.0\n";
-foreach ($_POST as $sNombre => $sValor)
-$sTexto = $sTexto."\n".$sNombre." = ".$sValor;
-foreach ($_FILES as $vAdjunto)
-{
-if ($bHayFicheros == 0)
-{
-$bHayFicheros = 1;
-$sCabeceras .= "Content-type: multipart/mixed;";
-$sCabeceras .= "boundary=\"--_Separador-de-mensajes_--\"\n";
-$sCabeceraTexto = "----_Separador-de-mensajes_--\n";
-$sCabeceraTexto .= "Content-type: text/plain;charset=iso-8859-1\n";
-$sCabeceraTexto .= "Content-transfer-encoding: 7BIT\n";
-$sTexto = $sCabeceraTexto.$sTexto;
-}
-if ($vAdjunto["size"] > 0)
-{
-$sAdjuntos .= "\n\n----_Separador-de-mensajes_--\n";
-$sAdjuntos .= "Content-type: ".$vAdjunto["type"].";name=\"".$vAdjunto["name"]."\"\n";;
-$sAdjuntos .= "Content-Transfer-Encoding: BASE64\n";
-$sAdjuntos .= "Content-disposition: attachment;filename=\"".$vAdjunto["name"]."\"\n\n";
-$oFichero = fopen($vAdjunto["tmp_name"], 'r');
-$sContenido = fread($oFichero, filesize($vAdjunto["tmp_name"]));
-$sAdjuntos .= chunk_split(base64_encode($sContenido));
-fclose($oFichero);
-}
-}
-if ($bHayFicheros)
-$sTexto .= $sAdjuntos."\n\n----_Separador-de-mensajes_----\n";
-return(mail($sPara, $sAsunto, $sTexto, $sCabeceras));
-}
-//cambiar aqui el email
-if (form_mail("ofjipizc@gmail.com", $_POST[asunto],
-"Los datos introducidos en el formulario son:\n\n", $_POST[email]))
-echo "
- <h1>Su formulario fue enviado con exito </h1>
-<form>
-<p>Muchas gracias <br>
-O lo que tu quieras poner los que sea!<br>
-Solo realiza el cambio en este texto!<br>
-<br>
-Saludos el equipo de ..lo que seas..
-</p>
-</form>
-";
+﻿<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$empresa = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . "\r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . "\r\n";"
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por" . $no,bre . ",\r\n";
+$mensaje .= "Su e-mail es:" . $mail . "\r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . "\r\n";
+$mensaje .= "Enviado el " . date('d/m'Y', time());
+
+$para = 'ofjipizc@gmail.com';
+$asunto = 'Asunto del mail recibido';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
 ?>
-    </div>
-</body>
-</html>
