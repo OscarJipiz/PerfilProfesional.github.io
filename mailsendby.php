9acf24bfb92b65c0705761b2eac9f50e98204db5
Oscar Jipiz Castro <32991695+OscarJipiz@users.noreply.github.com>
2017-11-25 21:27:16 -0500

    Add files via upload

--- mailsendby.php
+++ mailsendby.php
@@ -1,36 +1,65 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Confirmacion de envio formulario</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
 <?php
-if(isset($_POST['email'])) {
-
-// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "ofjipizc@gmail.com";
-$email_subject = "Contacto desde el sitio web";
-
-// Aquí se deberían validar los datos ingresados por el usuario
-if(!isset($_POST['first_name']) ||
-!isset($_POST['last_name']) ||
-!isset($_POST['email']) ||
-!isset($_POST['telephone']) ||
-!isset($_POST['comments'])) {
-
-echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-die();
+function form_mail($sPara, $sAsunto, $sTexto, $sDe)
+{
+$bHayFicheros = 0;
+$sCabeceraTexto = "";
+$sAdjuntos = "";
+if ($sDe)$sCabeceras = "From:".$sDe."\n";
+else $sCabeceras = "";
+$sCabeceras .= "MIME-version: 1.0\n";
+foreach ($_POST as $sNombre => $sValor)
+$sTexto = $sTexto."\n".$sNombre." = ".$sValor;
+foreach ($_FILES as $vAdjunto)
+{
+if ($bHayFicheros == 0)
+{
+$bHayFicheros = 1;
+$sCabeceras .= "Content-type: multipart/mixed;";
+$sCabeceras .= "boundary=\"--_Separador-de-mensajes_--\"\n";
+$sCabeceraTexto = "----_Separador-de-mensajes_--\n";
+$sCabeceraTexto .= "Content-type: text/plain;charset=iso-8859-1\n";
+$sCabeceraTexto .= "Content-transfer-encoding: 7BIT\n";
+$sTexto = $sCabeceraTexto.$sTexto;
 }
-
-$email_message = "Detalles del formulario de contacto:\n\n";
-$email_message .= "Nombre: " . $_POST['first_name'] . "\n";
-$email_message .= "Apellido: " . $_POST['last_name'] . "\n";
-$email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Teléfono: " . $_POST['telephone'] . "\n";
-$email_message .= "Comentarios: " . $_POST['comments'] . "\n\n";
-
-
-// Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
-
-echo "¡El formulario se ha enviado con éxito!";
+if ($vAdjunto["size"] > 0)
+{
+$sAdjuntos .= "\n\n----_Separador-de-mensajes_--\n";
+$sAdjuntos .= "Content-type: ".$vAdjunto["type"].";name=\"".$vAdjunto["name"]."\"\n";;
+$sAdjuntos .= "Content-Transfer-Encoding: BASE64\n";
+$sAdjuntos .= "Content-disposition: attachment;filename=\"".$vAdjunto["name"]."\"\n\n";
+$oFichero = fopen($vAdjunto["tmp_name"], 'r');
+$sContenido = fread($oFichero, filesize($vAdjunto["tmp_name"]));
+$sAdjuntos .= chunk_split(base64_encode($sContenido));
+fclose($oFichero);
 }
+}
+if ($bHayFicheros)
+$sTexto .= $sAdjuntos."\n\n----_Separador-de-mensajes_----\n";
+return(mail($sPara, $sAsunto, $sTexto, $sCabeceras));
+}
+//cambiar aqui el email
+if (form_mail("ofjipizc@gmail.com", $_POST[asunto],
+"Los datos introducidos en el formulario son:\n\n", $_POST[email]))
+echo "
+ <h1>Su formulario fue enviado con exito </h1>
+<form>
+<p>Muchas gracias <br>
+O lo que tu quieras poner los que sea!<br>
+Solo realiza el cambiio en este texto!<br>
+<br>
+Saludos el equipo de ..lo que seas..
+</p>
+</form>
+";
 ?>
+    </div>
+</body>
+</html>
